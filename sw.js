c73b5270dde49b3689cf594dc8e19da491047282
Nour S Y <nour.saif.yassine@gmail.com>
2026-02-07 20:14:40 +0100

    add: install

--- sw.js
+++ sw.js
@@ -1,12 +1,16 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/script.js'
-      ]);
+      return fetch('/manifest.json')
+        .then(() => cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/script.js'
+        ]))
+        .catch(() => {
+          console.log('Some files failed to cache');
+        });
     })
   );
 });
